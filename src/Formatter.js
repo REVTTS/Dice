dd5e990a959b9a52355c31908e824571364060d5
Ronald Zielaznicki <ronald.m.zielaznicki@gmail.com>
2022-09-03 22:18:11 -0400

    added copyright notice to Formatter.js

--- src/Formatter.js
+++ src/Formatter.js
@@ -1,3 +1,16 @@
+/*
+  Copyright (C) 2022 REVTTS, Ronald M Zielaznicki
+  This file is part of REVTTS Dice.
+  REVTTS Dice is free software: you can redistribute it and/or modify it under the terms of
+  the GNU General Public License as published by the Free Software Foundation, either
+  version 3 of the License, or any later version.
+  REVTTS Dice is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE. See the GNU General Public License for more details.
+  You should have received a copy of the GNU General Public License along with REVTTS Dice.
+  If not, see <https://www.gnu.org/licenses/>. 
+*/
+
 export class Formatter {
   constructor() {}
 
